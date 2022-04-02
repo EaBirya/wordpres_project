79095a5e8a5bab148a887fd1431710a1ad3a0075
EaBirya <19valdis94@gmail.com>
2022-04-03 01:26:18 +0300

    add: animation, form @media for all size fix bugs

--- src/js/script.js
+++ src/js/script.js
@@ -20,4 +20,66 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    new WOW().init();
+
+    $('[data-modal="consultation"]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+  
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks').fadeOut('slow');
+    });
+
+    
+   
+    $('#consultation form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+                },
+            phone: "required"
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите Ваше имя",
+                minlength: jQuery.validator.format("Минимально {0} символа")
+            },
+            phone: "Пожалуйста, введите Ваш номер телефона"
+            }
+    });
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+
+    function postForm(post_form) {
+        $(post_form).submit(function(e) {
+            e.preventDefault();
+    
+            if (!$(this).valid()) {
+                return;
+            }
+    
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+                $('#consultation').fadeOut();
+                $('.overlay , #thanks').fadeIn('slow');
+                $('.overlay , #thanks').delay(5000).fadeOut('slow');
+                $('form').trigger('reset');
+            });
+            return false;
+        });
+    };
+
+    postForm('#consultation-form')
+    postForm('#consultation form')
+
+
+    
 });
